cdc60f6dcbc1c2b4fe190272dc54f31e068e9d7c
Joel Duscha <jfduscha@gmail.com>
2026-01-20 17:14:05 +0100

    saving progress

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./features/home/home/home";
+import { Login } from './features/auth/login/login';
+import { Regsister } from './features/auth/regsister/regsister';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Login, Regsister],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-firebase-dienstplan-wh');
---
+++ src/app/features/auth/regsister/regsister.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regsister',
+  imports: [],
+  templateUrl: './regsister.html',
+})
+export class Regsister {
+
+}
