712205b63bb8a9893e2ec502504997457f2c8e68
Joel Duscha <jfduscha@gmail.com>
2026-01-20 17:29:43 +0100

    fixed warnings and prepared the routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then(m => m.Login),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home/home').then(m => m.Home),
+  },
+];
