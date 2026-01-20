d4310afe6fea0ed0890eddaf5ab7aef1d3ea3e4e
Joel Duscha <jfduscha@gmail.com>
2026-01-20 14:07:06 +0100

    hamburger menu without alpine

--- src/app/features/home/navbar/navbar.ts
+++ src/app/features/home/navbar/navbar.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './navbar.html',
 })
 export class Navbar {
+  open = false;
 
+  toggle(): void {
+    this.open = !this.open;
+  }
+
+  close(): void {
+    this.open = false;
+  }
 }
